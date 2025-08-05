d6715876328d2166359934b59d07b6a5d0d16f6e
Brayan Sandí Barrantes <sandi.bryansteef22@gmail.com>
2025-08-05 09:12:16 -0600

    Se agrega controlador con 2 endpoints Get

---
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,48 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> couponsList = _db.Coupons.ToList();
+                return couponsList;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error retrieving data: {ex.Message}");
+            }
+        }
+
+        [HttpGet]
+        [Route("{couponId:int}")]
+        public object Get(int couponId)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(c => c.CouponId == couponId);
+                return coupon;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error retrieving data: {ex.Message}");
+            }
+        }
+    }
+}
